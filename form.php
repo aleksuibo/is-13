57f39e5ed121f84aefe0cf7eb4694bcc116d8abc
aleksuibo <aleks547@hotmail.com>
2016-06-24 12:03:51 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,17 +1,6 @@
 <?php
 
         require "api.php";
-        
-		$data = array(
-                "username" => $_POST['username'],
-				"password" => $_POST['password'],
-                "product" => $_POST['product'],
-                "email" => $_POST['email'],
-				"phonenumber" => $_POST['phonenumber'],
-				"price" => $_POST['price'],
-				"action" => $_POST['action'],
-                "description" => $_POST['description'],
-                "pilt" => $_FILES['img']['tmp_name']);
 
         if (isset($_GET["id"])) {
                 $data["id"] = $_GET["id"];
